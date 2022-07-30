22309998ff3a9ac44108c6fea98c0ba1f3992c0f
Rodrigo Carreira <110118086+rodrigodcarreira@users.noreply.github.com>
2022-07-30 15:01:49 +0100

    Fixing SQLite_Busy Exception

--- plugin-minecraft/src/com/cash/events/Events.java
+++ plugin-minecraft/src/com/cash/events/Events.java
@@ -2,9 +2,14 @@ package com.cash.events;
 
 import Classes.User;
 import dbConnector.Connector;
+import java.util.Random;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
@@ -13,20 +18,45 @@ import org.bukkit.event.player.PlayerJoinEvent;
  */
 public class Events implements Listener {
 
+    private static Connector conn = new Connector();
+
     @EventHandler
     public static void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        String name = player.getName();
-        Connector conn = new Connector();
-        User user = conn.registUser(name);
-        
+
+        User user = conn.getUser(player.getName());
+
         if (user != null) {
-            if (user.isFirst()) {
-                player.sendMessage("Welcome to the server " + name);
-                player.sendMessage("You have 0 coins. Kill some mobs to get more!!!");
-            } else {
-                player.sendMessage("Welcome again " + name);
-                player.sendMessage("You have 0 coins.");
+            player.sendMessage("Welcome again " + player.getName());
+            player.sendMessage("You have " + user.getCoins() + " coins.");
+        } else {
+            conn.RegistUser(player.getName());
+            player.sendMessage("Welcome to the server " + player.getName());
+            player.sendMessage("You have 0 coins. Kill some mobs to get more!!!");
+        }
+
+    }
+
+    @EventHandler
+    public static void onEntityKill(EntityDeathEvent event) {
+
+        Entity killer = event.getEntity().getKiller();
+        Entity dead = event.getEntity();
+
+        if (killer instanceof Player) {
+            Player player = (Player) killer;
+            if (dead instanceof Creature) {
+                if (dead instanceof Monster) {
+                    Random random = new Random();
+                    if (conn.addCoins(player.getName(), random.nextInt(1, 3))) {
+                        User user = conn.getUser(player.getName());
+                        player.sendMessage("You have " + user.getCoins() + " coins !");
+                    } else {
+                        player.sendMessage("Error");
+                    }
+                } else {
+                    player.sendMessage("Don`t Kill None Hostile Mobs Please!");
+                }
             }
         }
 
--- plugin-minecraft/src/dbConnector/Connector.java
+++ plugin-minecraft/src/dbConnector/Connector.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -14,31 +15,125 @@ import java.sql.ResultSet;
 public class Connector {
 
     private String jbdcURL = "jdbc:sqlite:/C:\\SQLite\\data.db";
-    private Connection conn;
 
     public Connector() {
-        try {
-            conn = DriverManager.getConnection(jbdcURL);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
     }
 
-    public User registUser(String name) {
+    public User RegistUser(String name) {
+        Connection conn = null;
+        PreparedStatement pstm = null;
+        String sql = "INSERT INTO users(name, coins) VALUES(?,?);";
+
         try {
-            Statement stm = conn.createStatement();
-            ResultSet result = stm.executeQuery("SELECT * FROM users WHERE name like '" + name + "';");
-            while (result.next()) {
-                return new User(result.getInt("id"), name, result.getInt("coins"), false);
-            }
-            stm.close();
-            stm = conn.createStatement();
-            stm.execute("INSERT INTO users(name, coins) VALUES ('" + name + "', 0);");
+            conn = this.connect();
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, name);
+            pstm.setInt(2, 0);
+
+            pstm.executeUpdate();
             return new User(0, name, 0, true);
+        } catch (Exception e) {
+
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pstm != null) {
+                try {
+                    pstm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public User getUser(String name) {
+        Connection conn = null;
+        Statement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = connect();
+            stm = conn.createStatement();
+            rs = stm.executeQuery("SELECT * FROM users WHERE name like '" + name + "';");
+            while (rs.next()) {
+                return new User(rs.getInt("id"), name, rs.getInt("coins"), false);
+            }
         } catch (SQLException ex) {
             ex.printStackTrace();
             return null;
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (stm != null) {
+                try {
+                    stm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
+        return null;
+    }
+
+    public boolean addCoins(String name, int coins) {
+        Connection conn = null;
+        PreparedStatement pstm = null;
+        String sql = "UPDATE users SET coins = coins + ? WHERE name like ?;";
+
+        try {
+            conn = this.connect();
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, coins);
+            pstm.setString(2, name);
+            pstm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pstm != null) {
+                try {
+                    pstm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+    private Connection connect() {
+        try {
+            Connection conn = DriverManager.getConnection(jbdcURL);
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
